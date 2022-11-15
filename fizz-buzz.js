81cd61a108b5a93b0988545bf84396c3a34237e4
DMarrs <diegomarrs@gmail.com>
2022-11-15 12:54:51 -0800

    fizzbuzz

---
+++ fizz-buzz.js
@@ -0,0 +1,18 @@
+// A typical round of Fizz Buzz can be: 
+// Write a program that prints the numbers from 1 to 100 and for multiples of ‘3’ print “Fizz” instead of the number and for the multiples of ‘5’ print “Buzz”. 
+
+const fizzBuzz = () => {
+    for (let i = 1; i < 100; i++) {
+        if (i % 15 === 0) {
+            console.log('Fizz Buzz')
+        } else if (i % 3 === 0) {
+            console.log('Fizz');
+        } else if (i % 5 === 0) {
+            console.log('Buzz');
+        } else {
+            console.log(i)
+        }
+    }
+}
+
+fizzBuzz()
