ce8d4814be7a0548be6eb1624fb9ebc43b5e22c3
DMarrs <diegomarrs@gmail.com>
2022-11-15 12:55:48 -0800

    minor but necessary change

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -2,7 +2,7 @@
 // Write a program that prints the numbers from 1 to 100 and for multiples of ‘3’ print “Fizz” instead of the number and for the multiples of ‘5’ print “Buzz”. 
 
 const fizzBuzz = () => {
-    for (let i = 1; i < 100; i++) {
+    for (let i = 1; i <= 100; i++) {
         if (i % 15 === 0) {
             console.log('Fizz Buzz')
         } else if (i % 3 === 0) {
