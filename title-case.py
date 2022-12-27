4d732a7f6b0d9e1fa013290875ab23bd787e0fe8
DMarrs <diegomarrs@gmail.com>
2022-12-26 16:06:55 -0800

    title case complete

--- title-case.py
+++ title-case.py
@@ -2,3 +2,10 @@ def titleCase(str):
     # split the string up
     # iterate through it and capitalize each item
     # join the string again
+    splitString = str.split(' ');
+
+    capitalized = [word.capitalize() for word in splitString];
+
+    return " ".join(capitalized)
+
+print(titleCase("the quick brown fox jumped over the lazy dog"))
