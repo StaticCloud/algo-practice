5ec4953ff756007c0419df8679e5614dd8a08bdf
DMarrs <diegomarrs@gmail.com>
2022-12-26 16:23:21 -0800

    product of largest two pseudocode

---
+++ productOfLargestTwoi.py
@@ -0,0 +1,5 @@
+def productOfLargestTwo(arr):
+    # have a largest number two really small numbers
+    # compare an item in the array against the first item in the largest number array
+    # if it's smaller than the first item, compare it against the second item
+    # replace both numbers if they're respectively smaller than the items in the array
