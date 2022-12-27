a0551ff2964e4724fbb6daf0659d0bdbd7c45d04
DMarrs <diegomarrs@gmail.com>
2022-12-27 13:06:46 -0800

    added camelcase

--- camelCase.py
+++ camelCase.py
@@ -2,3 +2,13 @@ def camelCase(str):
     # split the string between spaces
     # set the first character to lower case
     # iterate through the rest of the strings and set them to uppercase
+    splitString = str.split(' ');
+
+    splitString[0] = splitString[0].lower();
+
+    for index in range(1, len(splitString)):
+        splitString[index] = splitString[index].capitalize();
+
+    return ''.join(splitString);
+
+print(camelCase("this is the end"))
