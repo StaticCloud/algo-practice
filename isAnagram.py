04b04bc7e043db547954d2001e6359ac17b65cac
DMarrs <diegomarrs@gmail.com>
2022-12-30 11:17:14 -0800

    added comments to wrong file lol

---
+++ isAnagram.py
@@ -0,0 +1,6 @@
+def isAnagram(strA, strB):
+    # create a dictionary with unique keys
+    # iterate over the first string and add each letter to the dictionary
+    # do the same with the second string
+    # them check if each value in the dictionary is 2
+    
