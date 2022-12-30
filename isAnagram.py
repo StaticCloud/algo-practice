8492f581f6215498e56c15e1a90c74e344d2a600
DMarrs <diegomarrs@gmail.com>
2022-12-30 11:47:00 -0800

    extra pseudocode and first attempt

--- isAnagram.py
+++ isAnagram.py
@@ -1,6 +1,36 @@
 def isAnagram(strA, strB):
-    # create a dictionary with unique keys
-    # iterate over the first string and add each letter to the dictionary
-    # do the same with the second string
-    # them check if each value in the dictionary is 2
+    # create one dictionary with unique keys
+    # update respective values with number of times a letter appears
+    # convert either dictionary to an array
+    # iterate through it and use .get() to retrieve the respective letter's value from each dictionary
+    # if it returns nothing, return false
+    anagramDictA = {};
+    anagramDictB = {};
+
+    for letter in strA:
+        if anagramDictA.get(letter) == None:
+            anagramDictA[letter] = 1;
+        else: 
+            anagramDictA[letter] = anagramDictA[letter] + 1;
+
+    for letter in strB:
+        if anagramDictB.get(letter) == None:
+            anagramDictB[letter] = 1;
+        else: 
+            anagramDictB[letter] = anagramDictB[letter] + 1;
+
+    lettersA = list(anagramDictA.keys());
+    lettersB = list(anagramDictB.keys());
+
+    for letter in lettersA:
+        if anagramDictA.get(letter) != anagramDictB.get(letter):
+            return False;
+
+    for letter in lettersB:
+        if anagramDictA.get(letter) != anagramDictB.get(letter):
+            return False;
+
+    return True;
     
+
+print(isAnagram("listen", "silent"))
