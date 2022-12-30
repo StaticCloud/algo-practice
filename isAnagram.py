daeb5f26adc3de791cc6915111a4940781fc1f48
DMarrs <diegomarrs@gmail.com>
2022-12-30 11:52:08 -0800

    added isanagram solution

--- isAnagram.py
+++ isAnagram.py
@@ -2,6 +2,12 @@ def isAnagram(strA, strB):
     # compare the length of the strings, if they're not the same return False
     # sort the strings using .sorted()
     # then compare if the two strings
-    
+    if len(strA) != len(strB):
+        return False;
+
+    sorted_A = sorted(strA);
+    sorted_B = sorted(strB);
+
+    return sorted_A == sorted_B;
 
 print(isAnagram("listen", "silent"))
