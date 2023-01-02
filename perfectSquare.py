cdaa0b37a5472cd5c391ddc428f2215cd4419c12
DMarrs <diegomarrs@gmail.com>
2023-01-02 09:19:18 -0800

    added perfect square

--- perfectSquare.py
+++ perfectSquare.py
@@ -2,3 +2,10 @@ def isPerfectSquare(num):
     # iterate numbers from 1 to num
     # veritfy if a number to the power of 2 is equal to num
     # if so return true
+    for number in range(num):
+        if number**2 == num:
+            return True
+
+    return False
+
+print(isPerfectSquare(36))
