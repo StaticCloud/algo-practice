a8ac954e38898b827cf495e525fedb4860ed3d69
DMarrs <diegomarrs@gmail.com>
2023-01-02 09:00:33 -0800

    added most songs pseudocode

---
+++ mostSongs.py
@@ -0,0 +1,4 @@
+def mostSongs(arr):
+    # sort the array so finding the most songs is easier
+    # add a two counters, one for the total minute and one for total songs
+    # if the total minutes exceeds 60, return the total songs
