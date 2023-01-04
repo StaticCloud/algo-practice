e15d990533a28c12028030121899cf6d3a34b295
DMarrs <diegomarrs@gmail.com>
2023-01-04 10:50:37 -0800

    added solution for 2d search

--- arraySearch2d.py
+++ arraySearch2d.py
@@ -2,3 +2,6 @@ def arraySearch2D(arr):
     # use list comprehension to get a 1D array
     # append items to a new array if item equals "x"
     # return the length
+    return len(list(filter(lambda char: char == "X", [item for array_1d in arr for item in array_1d])))
+
+print(arraySearch2D([["X"], ["X", "O", "X", "O"], ["X"]]))
