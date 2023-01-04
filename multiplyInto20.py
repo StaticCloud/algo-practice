c2bf8cb5651aa443e67f398468cefc729ae9bb8b
DMarrs <diegomarrs@gmail.com>
2023-01-04 10:59:25 -0800

    added pseudocode for multiplyinto20

---
+++ multiplyInto20.py
@@ -0,0 +1,5 @@
+def multiplyInto20(arr):
+    # compare the first item in the array to every item
+    # then pop it and compare everything with the new first item
+    # if there are two numbers in the array that multiply into 20, return true
+    # otherwise return false
