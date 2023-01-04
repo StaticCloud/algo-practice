b6603e749c376389ff325ae6f8a8c27115c794be
DMarrs <diegomarrs@gmail.com>
2023-01-04 11:15:04 -0800

    added multiplyinto20 sol

--- multiplyInto20.py
+++ multiplyInto20.py
@@ -3,3 +3,15 @@ def multiplyInto20(arr):
     # then pop it and compare everything with the new first item
     # if there are two numbers in the array that multiply into 20, return true
     # otherwise return false
+    while len(arr):
+        firstItem = arr[0]
+
+        for num in range(1, len(arr)):
+            if firstItem * arr[num] == 20:
+                return True;
+        
+        arr.pop(0)
+    
+    return False;
+
+print(multiplyInto20([20, -20, 18, 2, 3, 4]))
