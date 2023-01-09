a93f4762566fabf2d38a446736b0e25de8387bec
DMarrs <diegomarrs@gmail.com>
2023-01-09 14:27:13 -0800

    pseudocode for merge sort

---
+++ mergeSorted.py
@@ -0,0 +1,5 @@
+def mergeSort(arr1, arr2):
+    # create unique index for both arrays
+        # is arr1[index1] > arr2[index2]
+        # push arr2[index2] to the new array
+        # otherwise push arr1[index1] to the new array
