cfe5632cdbe1696c0b870a81d60d8bd2842ea890
DMarrs <diegomarrs@gmail.com>
2023-01-11 14:50:29 -0800

    array intersect pseudocode

---
+++ arrayIntersection.py
@@ -0,0 +1,5 @@
+def arrayIntersection(arr1, arr2):
+    # determine which array to iterate over depending on which one is bigger
+    # iterate over the smaller array
+    # use if ... in to determine if an item exists in both arrays
+    # push it to an array we will return
