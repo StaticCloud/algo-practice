27171b3e7ec0c2450d862aef70aad15a81709fbc
DMarrs <diegomarrs@gmail.com>
2023-01-14 10:11:11 -0800

    added common element

---
+++ commonElement.py
@@ -0,0 +1,14 @@
+def commonElement(arr1, arr2):
+    elementDict = {}
+
+    for element in arr1:
+        if elementDict.get(element) == None:
+            elementDict[element] = True
+
+    for element in arr2:
+        if elementDict.get(element) == None:
+            elementDict[element] = True
+        else:
+            return element;
+
+print(commonElement([1, 9, 8, 7],[10, 12, 1, 6, 5]))
