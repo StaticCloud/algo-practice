a0157196cac216af43e1abef0370854fefe99800
DMarrs <diegomarrs@gmail.com>
2023-01-16 17:14:12 -0800

    added pseudocode for concert flyer

---
+++ concertFlyer.py
@@ -0,0 +1,4 @@
+def concertFlyer(magazine, flyer):
+    # split the flyer and the magazine between spaces
+    # loop through each string item in the flyer and verify if it exists in the magazine
+    # if not, return false
