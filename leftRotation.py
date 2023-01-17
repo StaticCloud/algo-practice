696407d832881343d5d62811489dd6f82101a73d
DMarrs <diegomarrs@gmail.com>
2023-01-17 14:35:44 -0800

    added left rotation pseudocode

--- leftRotation.py
+++ leftRotation.py
@@ -1,1 +1,4 @@
 def leftRotation(arr, positions):
+    # assign the positions variable to a counter variable
+    # move the first item in the array at the end of the array and reduce the counter
+    # repeat until the counter has reached 0
