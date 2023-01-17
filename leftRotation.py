79cc40da985e93d9ac65f76a28f00f3d1001d92b
DMarrs <diegomarrs@gmail.com>
2023-01-17 14:59:43 -0800

    added left rotation

--- leftRotation.py
+++ leftRotation.py
@@ -2,3 +2,13 @@ def leftRotation(arr, positions):
     # assign the positions variable to a counter variable
     # move the first item in the array at the end of the array and reduce the counter
     # repeat until the counter has reached 0
+    while positions > 0:
+        first = arr[0]
+        arr = arr[1:]
+        arr.append(first)
+        positions -= 1
+        
+
+    return arr
+
+print(leftRotation([1, 2, 3], 2))
