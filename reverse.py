b8330b709931d0920c54708f93d17dfe1b4efb49
DMarrs <diegomarrs@gmail.com>
2023-01-17 12:59:56 -0800

    added reverse

--- reverse.py
+++ reverse.py
@@ -1,3 +1,13 @@
 def reverse(string):
     # iterate through the string from the end of the string
     # append each item to a new string, which will be our reversed string
+    strLen = len(string) - 1
+    reversedStr = ""
+
+    while strLen >= 0:
+        reversedStr = reversedStr + string[strLen]
+        strLen = strLen - 1;
+
+    return reversedStr
+
+print(reverse("just keep swimming"))
