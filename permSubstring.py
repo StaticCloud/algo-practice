aaa1f9463d311870f2dcb48e6c5ded88ee3a6ce7
DMarrs <diegomarrs@gmail.com>
2023-01-16 17:09:36 -0800

    pseudocode perm substring

---
+++ permSubstring.py
@@ -0,0 +1,4 @@
+def permSubstring(str, sub):
+    # loop through each element in the string
+    # if the respective letter is in substring, continue the loop
+    # however, if it's not, immediatly return false
